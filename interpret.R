1e73efaa45268c2ea12717f00dbfde05cbeef012
brianc0413 <59323159+brianc0413@users.noreply.github.com>
2021-11-02 03:09:08 -0400

    Add files via upload
    Rough skeletons of PDP/ICE methods for continuous variables

---
+++ interpret.R
@@ -0,0 +1,225 @@
+# Generalized Prediction Methods
+library(R6)
+library(ggplot2)
+library(data.table)
+source("predictor.R")
+
+#' PDP and ICE Implementation
+#' similar methods
+#' 
+#' 
+#' Needed fixes: set predictions as a one time calculation, allow for specific
+#' points instead of number of points, add in second variable interpretations, 
+#' add grouping by another variable
+
+
+Interpreter <- R6Class("Interpreter",
+  public = list(
+    feature = NULL,
+    n.features = NULL, # for future implementation of two variable
+    predictor = NULL,
+    grid.points = NULL,
+    center.at = NULL,
+    group = NULL,
+    data.points = NULL,
+
+    # initialize an interpreter object
+    initialize = function(predictor, feature, center.at=NULL,
+                         grid.size=20, grid.points=NULL, group = NULL, samples = NULL){
+      
+      # check to see if predictor is a valid predictor object
+      if (!(inherits(predictor, "Predictor"))){
+        stop("Predictor given is not of the Predictor class.")
+      }
+      
+      # check to see if feature is a valid feature (only numeric)
+      if (length(feature) > 1){
+        if (method=="ale"){
+          stop("ALE interpretation methods only support 1 variable.")
+        } else {
+          stop("Only up to two features are allowed. Currently working on two feature implementation!")
+        }
+      }
+      if (any(!(feature %in% names(predictor$data)))){
+        stop("Feature(s) is/are not in data.")
+      }
+      if (any(feature == predictor$y)){
+        stop("Feature selected is the output. Please input a valid feature.")
+      }
+      if (!(inherits(predictor$data[,feature], "numeric"))){
+        stop("Feature is non-numeric. Please give a numeric feature.")
+      }
+      if (length(unique(predictor$data[,feature]))<2){
+        stop("There is only a single value that occurs for this feature.")
+      }
+      
+      # check to see if valid grid.points
+      if (is.null(grid.points)){
+        grid.points <- seq(min(predictor$data[,feature]), 
+                           max(predictor$data[,feature]),
+                           length.out = grid.size)
+      } else {
+        checkmate::assert_numeric(grid.points)
+        grid.points <- unique(grid.points)
+        if (length(grid.points) < 2){
+          stop("Please give at least two distinct grid points.")
+        }
+      }
+      
+      # check to see if valid center.at input
+      checkmate::assert_numeric(center.at, null.ok = TRUE)
+      if (is.null(center.at)){
+          center.at <- min(grid.points)
+      } else {
+        if (center.at > max(self$grid.points) | center.at < min(self$grid.points)){
+          stop("Centered point is not within boundaries of grid points.")
+        }
+      } 
+      
+      # check to see if valid group, only allows for one group at the moment
+      if (!is.null(group)){
+        # check that the variable is in the list of variables
+        if (group == predictor$y || 
+            !(group %in% names(predictor$data))){
+          stop("Invalid variable given for groups.")
+        } else {
+          if (class(data[, group]) != "factor"){
+            stop("The grouping must be done by a factor variable.")
+          }
+        }
+      }
+      
+      # check to see if valid sample number
+      data <- predictor$data
+      if (is.null(samples)){
+        # set samples to either batch size or whole dataset
+        N <- predictor$batch.size
+        N <- min(N, nrow(data))
+        samples <- N
+      } else {
+        # get number of datapoints equal to samples
+        checkmate::assert_numeric(samples)
+        samples <- as.integer(samples)
+        if (samples > nrow(samples) | samples < 1){
+          stop("Invalid sample number.")
+        }
+      }
+      # set data.points to be worked with (# of datapoints = sample number)
+      data.points <- sample(1:nrow(data), samples)
+      
+      # initialize all variables belonging to this class
+      self$feature <- feature
+      self$n.features <- length(feature)
+      self$predictor <- predictor
+      self$grid.points <- grid.points
+      self$center.at <- center.at
+      self$group <- group
+      self$data.points <- data.points
+    },
+    
+    # can go back and change the scale of the gridpoints
+    set.gridpoints = function(grid.points){
+      checkmate::assert_numeric(grid.points)
+      grid.points <- unique(grid.points)
+      if (length(grid.points) < 2){
+        stop("Please give at least two distinct grid points.")
+      }
+      self$grid.points <- grid.points
+    },
+    
+    # change center
+    set.center = function(center = NULL){
+      if (is.null(center)){
+        self$center.at <- min(self$grid.points)
+      } else {
+        checkmate::assert_numeric(center)
+        if (center > max(self$grid.points) | center < min(self$grid.points)){
+          stop("Centered point is not within boundaries of grid points.")
+        }
+        self$center.at <- center
+      }
+    },
+    
+    # prediction function for grid points
+    predict_grid = function(){
+      # get the data for the selected samples
+      data.points <- self$data.points
+      data <- self$predictor$data[data.points, , drop=FALSE]
+      # make a table for results
+      results <- data.frame(sentinel = rep(0, length(data.points)))
+      for (val in self$grid.points){
+        newdata <- data
+        newdata[, self$feature] <- val
+        #return((self$predictor$predict(newdata)))
+        results <- cbind.data.frame(results, 
+                                    val = (self$predictor$predict(newdata))[,1])
+      }
+      results <- data.frame(results)
+      results <- results[,-1, drop = FALSE]
+      colnames(results) <- self$grid.points
+
+      # subtract out the centered value
+      newdata <- data
+      newdata[,self$feature] <- self$center.at
+      base <- self$predictor$predict(newdata)
+
+      # normalize results
+      results <- results - base[,1]
+      names.row <- rownames(data)
+
+      # return transpose (easier for plotting)
+      results <- (t(results))
+      colnames(results) <- names.row
+      results <- cbind(feature = self$grid.points, results)
+      rownames(results) <- NULL
+      
+      return(data.frame(results))
+    },
+    
+    plot = function(method = "pdp+ice"){
+      # grab the predictions dataframe
+      df <- self$predict_grid()
+      # df contains both pdp line and all ice lines
+      pdp.line <- rowMeans(df[,-which(colnames(df)=="feature")])
+      df <- cbind(df ,pdp = pdp.line)
+      df <- setDT(data.frame(df))
+      df.ice <- df[, -"pdp"]
+      
+      # for scaling
+      min.val <- min(df[,-"feature"])
+      max.val <- max(df[,-"feature"])
+
+      melt.df <- melt(df, id.vars = "feature")
+      melt.df.ice <- melt(df.ice, id.vars="feature")
+      
+      # check to make sure that the method given is valid
+      if (!is.null(self$group)){
+        stop("Will implement this feature later.")
+      }
+      if (!(method %in% c("pdp", "ice", "pdp+ice"))){
+        stop("Method entered is not supported")
+      }
+
+      if (method=="ice"){
+        plot.obj <- ggplot(data=melt.df.ice, aes(x=feature, y=value, group=variable)) + 
+          geom_line(color="grey")
+      }
+
+      if (method == "pdp"){
+        plot.obj <- ggplot(data=melt.df[melt.df$variable=="pdp",], aes(x=feature,y=value)) + 
+          geom_line() 
+      }
+
+      if (method == "pdp+ice"){
+        melt.df.combined <- melt.df
+        melt.df.combined$ispdp <- (melt.df$variable=="pdp")
+        plot.obj <- ggplot(data=melt.df.combined, aes(x=feature, y=value,
+                                                      group=variable, color=ispdp)) +
+          geom_line() + scale_color_manual(values=c("grey", "red")) 
+      }
+      
+      return(plot.obj+ ylab(self$predictor$y) + xlab(self$feature) )
+    }
+  )                     
+                       
+  )
---
+++ predictor.R
@@ -0,0 +1,133 @@
+#' Predictor Object
+#' 
+#' A wrapper to pass through an ML algorithm (rpart, etc.) through the 
+#' interpretability functions, the data used to create the algorithm
+#'
+#' The necessary variables are model, data, y. The other variables are 
+#' optional, and depend on the use cases. Type should be used only when 
+#' a prediction function is NOT specified.
+#'
+#' The outputs of the algorithm must be the values if it is regression, or 
+#' probabilities if classification. For classification problems with more than 
+#' two categories, the output comes out as vectors of probabilities for each
+#' category. Because this is for ML interpretability, other types of 
+#' predictions (ex: predictions that spit out the factor) are not allowed.
+#'
+library(R6)
+Predictor <- R6Class("Predictor",
+    public = list(
+    data = NULL,
+    model = NULL,
+    task = NULL,
+    class = NULL,
+    prediction.function = NULL,
+    batch.size = NULL,
+    y = NULL,
+    
+    initialize = function(model=NULL, data=NULL, predict.func=NULL,
+                          y=NULL, task = NULL, class=NULL, type=NULL, 
+                          batch.size = 1000) {
+      
+      # check that batch size is at least 1 and number
+      checkmate::assert_number(batch.size, lower=1)
+      
+      # checks for model input
+      if (is.null(model)){
+        stop("Model not given.")
+      }
+      
+      # takes in data, makes sure that it is a dataframe by the end or stops
+      if (is.null(data)){
+        stop("Data not given.")
+        if (!inherits(data, "data.table") || !inherits(data, "data.frame")){
+          stop("Data is not a data.frame or data.table object.")
+        }
+      if (inherits(data, "data.table")){
+          data.table::setDF(data)
+        }
+      }
+      
+      # checks for valid y input
+      if (is.null(y) || !is.character(y) || !(y%in%names(data))){
+        stop("Y has not been given, is not a character variable, or is not a variable in the given data.")
+      }
+      
+      # checks for valid task
+      if (!is.null(task)){
+        if (!(task %in% c("regression", "classification"))){
+          stop("Only regression or classification tasks are supported.")
+        }
+      }
+      
+      #' prediction method must be constructed, with optional arguments of
+      #' type and task
+      if (is.null(predict.func)){
+        if (is.null(type)){
+          predict.func.final <- function(model, newdata) predict(model, newdata)
+        } else{
+          predict.func.final <- function(model, newdata) predict(model, newdata, type = type)
+        }
+      } else{
+        predict.func.final <- function(model, newdata){
+          pred <- do.call(predict.func, list(model, newdata = newdata))
+          data.frame(pred, check.names = FALSE)
+        }
+      }
+      
+      self$data <- data
+      self$class <- class
+      self$model <- model
+      self$task <- task
+      self$prediction.function <- predict.func.final
+      self$batch.size <- batch.size
+      self$y <- y
+    },
+    
+    predict = function(newdata){
+      # check that the new data is a dataframe
+      checkmate::assert_data_frame(newdata)
+      newdata <- as.data.frame(newdata)
+      
+      # drop Y variable to make sure that only features are used
+      if (self$y %in% names(newdata)){
+        newdata <- newdata[, -which(names(newdata) == self$y), drop = FALSE]
+      }
+      
+      # if we predict 1 target
+      preds <- self$prediction.function(self$model, newdata)
+      preds <- data.frame(preds)
+
+      # check for valid prediction
+      if (private$predictionCheck == FALSE){
+        # number of rows in predictions
+        if (nrow(predictions)!= nrow(newdata)){
+          stop("Number of predictions do not match the number of observations.")
+        }
+        # check for missing values
+        if (sum(is.na(predictions))!=0){
+          stop("Predictions have missing values.")
+        }
+        private$predictionCheck <- TRUE
+      }
+      
+      # Class variable (if classification)
+      if (!is.null(self$class) && ncol(predictions) > 1){
+        predictions <- predictions[, self$class, drop = FALSE]
+      }
+      rownames(predictions) <- NULL
+      return(predictions)
+    },
+    
+    print = function(){
+      cat("Prediction Task:", self$task, "\n")
+      if (self$task == "classification"){
+        cat("Classes: ", paste(unique(self$data[,which(names(self$data)==self$y)]), collapse=" "))
+      }
+    }
+    ),
+    private = list(
+      predictionCheck = FALSE
+    )
+    )
+  
+  
