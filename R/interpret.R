75061e5c3ed7b10bc80517e63693e278b96c8a36
brianc0413 <59323159+brianc0413@users.noreply.github.com>
2022-01-19 15:15:38 -0500

    Update interpret.R

--- R/interpret.R
+++ R/interpret.R
@@ -3,284 +3,191 @@
 
 #' @title The class for different interpretability methods.
 #' @description Interpreter class description
-#' @field feature Feature to be examined in the model
-#' @field n.features Number of features the model was trained on
 #' @field predictor The predictor object to use as a standardised wrapper for the model
-#' @field grid.points The points at which to run the predictions.
-#' @field center.at The place we center the predictions at.
-#' @field group A categorical group variable in order to stratify the PDP predictions by.
-#' @field data.points Data points to use for the predictions.
+#' @field data.points The indices of the data points used for the PDP/ALE. This
+#' overwrites the "samples" initialization or the batch size variable from the predictor class.
+#' @field functions.1d Functions giving average value across data.points with a given value and feature
+#' @field functions.2d Functions giving 2D pdp functions
+#' @field method The chosen interpretability method for the black-box model.
 #' @examples
-#' data <- MASS::Boston
-#' set.seed(491)
-#' test_ind <- sample(1:nrow(data), nrow(data)%/%5)
-#' train_reg <- data[-test_ind,]
-#' test_reg <- data[test_ind,]
-#'
-#'
-#' data_class <- MASS::crabs
-#' test_ind <- sample(1:nrow(data_class), nrow(data_class)%/%5)
-#' train_class <- data_class[-test_ind,]
-#' test_class <- data_class[test_ind,]
-#'
-#'
-#' linreg <- lm(crim ~., data=train_reg)
-#' linreg_predictor <- Predictor$new(model = linreg, data = train_reg, y="crim",
-#'                                   predict = predict, task = "regression")
-#' actual <- predict(linreg, test_reg)
-#' method <- linreg_predictor$predict(test_reg)
-#' sum(actual != method) # all the same values
-#'
-#' linreg_predictor$print()
-#'
-#' linreginterpret <- Interpreter$new(predictor = linreg_predictor,
-#'                                    feature = "indus")
-#'
-#' linreginterpret$plot()
+#' Needs to be rewritten
 #' @export
-Interpreter <- R6::R6Class("Interpreter",
+Interpreter <- R6::R6Class(
+  "Interpreter",
   public = list(
-    feature = NULL,
-    n.features = NULL, # for future implementation of two variable pdp's
     predictor = NULL,
-    grid.points = NULL,
-    center.at = NULL,
-    group = NULL,
+    features = NULL,
     data.points = NULL,
+    functions.1d = NULL,
+    functions.2d = NULL,
+    method = NULL,
+    # functions.ale = to come later
 
     # initialize an interpreter object
     #' @param predictor The trained predictor object for the model that we want to
     #'  interpret.
-    #' @param feature The name of the feature that we use for the interpretability model.
-    #' @param center.at The location we should use to center the predictions.
-    #' @param grid.size The grid size used for the PDP.
-    #' @param grid.points Number of grid points used in the PDP.
-    #' @param group A group variable to classify the observations and give more
-    #'   granular predictions.
-    #' @param samples The samples used for the PDP/ALE.
+    #' @param samples The samples used for the PDP/ALE. Overwrites the batch size
+    #' parameter from the predictor object.
+    #' @param data.points The indices of the data points used for the PDP/ALE. This
+    #' overwrites the "samples" parameter or the batch size from the predictor.
+    #' @param method The chosen interpretability method for the black-box model.
     #' @return An `Interpreter` object.
     #' @note
     #' PDP and ICE Implementation
-    #' similar methods
-    #' Needed fixes: set predictions as a one time calculation, allow for specific
-    #' points instead of number of points, add in second variable interpretations,
-    #' add grouping by another variable
-    initialize = function(predictor, feature, center.at=NULL,
-                          grid.size=20, grid.points=NULL, group = NULL, samples = NULL){
-
+    initialize = function(predictor = NULL,
+                          samples = NULL,
+                          data.points = NULL,
+                          method = "pdp") {
       # check to see if predictor is a valid predictor object
-      if (!(inherits(predictor, "Predictor"))){
+      if (is.null(predictor)) {
+        stop("Predictor not given.")
+      }
+      if (!(inherits(predictor, "Predictor"))) {
         stop("Predictor given is not of the Predictor class.")
       }
 
-      # check to see if feature is a valid feature (only numeric)
-      if (length(feature) > 1){
-        if (method=="ale"){
-          stop("ALE interpretation methods only support 1 variable.")
-        } else {
-          stop("Only up to two features are allowed. Currently working on two feature implementation!")
-        }
-      }
-      if (any(!(feature %in% names(predictor$data)))){
-        stop("Feature(s) is/are not in data.")
-      }
-      if (any(feature == predictor$y)){
-        stop("Feature selected is the output. Please input a valid feature.")
-      }
-      if (!(inherits(predictor$data[,feature], "numeric"))){
-        stop("Feature is non-numeric. Please give a numeric feature.")
-      }
-      if (length(unique(predictor$data[,feature]))<2){
-        stop("There is only a single value that occurs for this feature.")
-      }
+      # determine valid features
+      # remove y variable from consideration
+      possible <-
+        names(predictor$data)[names(predictor$data) != predictor$y]
+      # find valid features in the data: more than 1 value
+      possible <-
+        names(which(sapply(
+          sapply(predictor$data[, possible], unique), length
+        ) > 1))
+      # one of the following: int, factor, numeric
+      possible <-
+        possible[which(sapply(predictor$data[, possible], class) %in%
+                         c("numeric", "factor", "integer"))]
+      features <- possible
 
-      # check to see if valid grid.points
-      if (is.null(grid.points)){
-        grid.points <- seq(min(predictor$data[,feature]),
-                           max(predictor$data[,feature]),
-                           length.out = grid.size)
-      } else {
-        checkmate::assert_numeric(grid.points)
-        grid.points <- unique(grid.points)
-        if (length(grid.points) < 2){
-          stop("Please give at least two distinct grid points.")
-        }
-      }
-
-      # check to see if valid center.at input
-      checkmate::assert_numeric(center.at, null.ok = TRUE)
-      if (is.null(center.at)){
-          center.at <- min(grid.points)
-      } else {
-        if (center.at > max(self$grid.points) | center.at < min(self$grid.points)){
-          stop("Centered point is not within boundaries of grid points.")
-        }
-      }
-
-      # check to see if valid group, only allows for one group at the moment
-      if (!is.null(group)){
-        # check that the variable is in the list of variables
-        if (group == predictor$y ||
-            !(group %in% names(predictor$data))){
-          stop("Invalid variable given for groups.")
-        } else {
-          if (class(data[, group]) != "factor"){
-            stop("The grouping must be done by a factor variable.")
-          }
-        }
-      }
 
       # check to see if valid sample number
+      # if data.points is not specified
       data <- predictor$data
-      if (is.null(samples)){
-        # set samples to either batch size or whole dataset
-        N <- predictor$batch.size
-        N <- min(N, nrow(data))
-        samples <- N
-      } else {
-        # get number of datapoints equal to samples
-        checkmate::assert_numeric(samples)
-        samples <- as.integer(samples)
-        if (samples > nrow(samples) | samples < 1){
-          stop("Invalid sample number.")
+      if (is.null(data.points)) {
+        if (is.null(samples)) {
+          # set samples to either batch size or whole dataset
+          N <- predictor$batch.size
+          N <- min(N, nrow(data))
+          samples <- N
+        } else {
+          # get number of datapoints equal to samples
+          checkmate::assert_numeric(samples)
+          samples <- as.integer(samples)
+          if (samples > nrow(samples) | samples < 1) {
+            stop("Invalid sample number.")
+          }
+        }
+        # set data.points to be worked with (# of datapoints = sample number)
+        data.points <- sample(1:nrow(data), samples)
+      }
+      # if data points were specified
+      else {
+        checkmate::assert_numeric(data.points)
+        if (any(!(data.points %in% 1:nrow(data)))) {
+          stop("The data points are not in the training data.")
         }
       }
-      # set data.points to be worked with (# of datapoints = sample number)
-      data.points <- sample(1:nrow(data), samples)
+
+      # check that method is supported
+      checkmate::assert_character(method)
+      if (!(method %in% c("pdp", "ale"))) {
+        stop("This method is not supported by the function.")
+      }
+
+      # create prediction functions for the features
+      if (method == "pdp") {
+        # for 1-dimensional functions
+        pdp.function <- function(feature) {
+          force(feature)
+          pdp.function.2 <- function(val) {
+            data <- predictor$data[data.points, , drop = FALSE]
+            data[, feature] <- val
+            mean(predict(predictor, data)[, 1])
+          }
+          return(pdp.function.2)
+        }
+
+        functions.pdp <- list()
+        for (feature in features) {
+          temp.list <- list(pdp.function(feature = feature))
+          functions.pdp <- append(functions.pdp, temp.list)
+        }
+        names(functions.pdp) <- features
+
+        # for 2d features
+        pdp.function.2d <- function(feature.1, feature.2){
+          force(feature.1)
+          force(feature.2)
+          pdp.function.2d.2 <- function(val.1, val.2){
+            data <- predictor$data[data.points, , drop = FALSE]
+            data[, feature.1] <- val.1
+            data[, feature.2] <- val.2
+            mean(predict(predictor, data)[,1])
+          }
+          return(pdp.function.2d.2)
+        }
+        functions.pdp.2d <- list()
+        for (feature.1 in features){
+          # list of functions for each
+          temp.list <- list()
+          for (feature.2 in features){
+            if (feature.1 != feature.2){
+              temp <- pdp.function.2d(feature.1 = feature.1, feature.2 = feature.2)
+            }
+            else {
+              temp <- "Please use the one-dimensional functions."
+            }
+            temp.list <- append(temp.list, temp)
+          }
+          names(temp.list) <- features
+          functions.pdp.2d <- append(functions.pdp.2d, list(temp.list))
+        }
+        names(functions.pdp.2d) <- features
+      }
+      else {
+        stop("Other methods have not been implemented yet for general functions.")
+      }
 
       # initialize all variables belonging to this class
-      self$feature <- feature
-      self$n.features <- length(feature)
+      self$features <- features
       self$predictor <- predictor
-      self$grid.points <- grid.points
-      self$center.at <- center.at
-      self$group <- group
       self$data.points <- data.points
-    },
-
-    # can go back and change the scale of the gridpoints
-    #' @param grid.points The grid points to be used.
-    #' @export
-    set.gridpoints = function(grid.points){
-      checkmate::assert_numeric(grid.points)
-      grid.points <- unique(grid.points)
-      if (length(grid.points) < 2){
-        stop("Please give at least two distinct grid points.")
-      }
-      self$grid.points <- grid.points
-    },
-
-    # change center
-    #' @param center The value to center the predictions at
-    #' @export
-    set.center = function(center = NULL){
-      if (is.null(center)){
-        self$center.at <- min(self$grid.points)
-      } else {
-        checkmate::assert_numeric(center)
-        if (center > max(self$grid.points) | center < min(self$grid.points)){
-          stop("Centered point is not within boundaries of grid points.")
-        }
-        self$center.at <- center
-      }
+      self$functions.1d <- functions.pdp
+      self$functions.2d <- functions.pdp.2d
+      self$method <- method
     }
   )
 )
 
-#' @name predict_grid.Interpretor
-#' @title # prediction function for grid points
-#' @description Gives predictions at each point on the grid.
-#' @param object The Interpretor object to use.
-#' @export
-predict_grid.Interpreter = function(
-  object
-){
-  # get the data for the selected samples
-  data.points <- object$data.points
-  data <- object$predictor$data[data.points, , drop=FALSE]
-
-  # make a table for results
-  results <- data.frame(sentinel = rep(0, length(data.points)))
-
-  for (val in object$grid.points){
-    newdata <- data
-    newdata[, object$feature] <- val
-    results <- cbind.data.frame(results,
-                                val = predict(object$predictor, newdata)[,1])
+# allows user to set data.points
+set.data.points = function(object, data.points) {
+  checkmate::assert_numeric(data.points)
+  if (!(inherits(object, "Interpreter"))) {
+    stop("Object given is not of the Interpreter class.")
   }
-
-  results <- data.frame(results)
-  results <- results[,-1, drop = FALSE]
-  colnames(results) <- object$grid.points
-
-  # subtract out the centered value
-  newdata <- data
-  newdata[,object$feature] <- object$center.at
-  base <- predict(object$predictor,  newdata)
-
-  # normalize results
-  results <- results - base[,1]
-  names.row <- rownames(data)
-
-  # return transpose (easier for plotting)
-  results <- (t(results))
-  colnames(results) <- names.row
-  results <- cbind(feature = object$grid.points, results)
-  rownames(results) <- NULL
-
-  return(data.frame(results))
+  if (any(!(data.points %in% 1:nrow(object$predictor$data)))) {
+    stop("The data points are not in the data.")
+  }
+  return(Interpreter$new(object$predictor,
+                                object$features,
+                                data.points = data.points,
+                                object$method))
 }
 
-#' @name plot.Interpretor
-#' @title PLotting method for Interpretor model
-#' @description Plots either the PDP plots or ICE plots
-#' @param object Interpretor object to make plots for.
-#' @param method The type of plot to make. Can be one of `ice`,`pdp`,`pdp+ice`.
-#' @export
-plot.Interpreter = function(
-  object,
-  method = "pdp+ice"
-){
-  # grab the predictions dataframe
-  df <- predict_grid.Interpreter(object)
-  # df contains both pdp line and all ice lines
-  pdp.line <- rowMeans(df[,-which(colnames(df)=="feature")])
-  df <- cbind(df ,pdp = pdp.line)
-  df <- setDT(data.frame(df))
-  df.ice <- df[, -"pdp"]
-
-  # for scaling
-  min.val <- min(df[,-"feature"])
-  max.val <- max(df[,-"feature"])
-
-  melt.df <- melt(df, id.vars = "feature")
-  melt.df.ice <- melt(df.ice, id.vars="feature")
-
-  # check to make sure that the method given is valid
-  if (!is.null(object$group)){
-    stop("Will implement this feature later.")
+# allows user to set type of interpretability method
+set.method = function(object, method){
+  checkmate::assert_character(method)
+  if (!(inherits(object, "Interpreter"))) {
+    stop("Object given is not of the Interpreter class.")
   }
-  if (!(method %in% c("pdp", "ice", "pdp+ice"))){
-    stop("Method entered is not supported")
+  if (!(method %in% c("pdp", "ale"))){
+    stop("This method is not supported by the function.")
   }
-
-  if (method=="ice"){
-    plot.obj <- ggplot(data=melt.df.ice, aes(x=feature, y=value, group=variable)) +
-      geom_line(color="grey")
-  }
-
-  if (method == "pdp"){
-    plot.obj <- ggplot(data=melt.df[melt.df$variable=="pdp",], aes(x=feature,y=value)) +
-      geom_line()
-  }
-
-  if (method == "pdp+ice"){
-    melt.df.combined <- melt.df
-    melt.df.combined$ispdp <- (melt.df$variable=="pdp")
-    plot.obj <- ggplot(data=melt.df.combined, aes(x=feature, y=value,
-                                                  group=variable, color=ispdp)) +
-      geom_line() + scale_color_manual(values=c("grey", "red"))
-  }
-
-  return(plot.obj+ ylab(object$predictor$y) + xlab(object$feature) )
+  return(Interpreter$new(object$predictor,
+                                object$features,
+                                object$data.points,
+                                method = method))
 }
+
