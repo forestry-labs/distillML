af7349ffc41acf504b098c14a812aa5de7ea9df4
brianc0413 <59323159+brianc0413@users.noreply.github.com>
2022-01-19 16:00:16 -0500

    Update interpret.R

--- R/interpret.R
+++ R/interpret.R
@@ -1,191 +1,204 @@
-#' Predictor Object
+#' @include predictor.R
 #' @importFrom R6 R6Class
 
-#' @title The class that wraps a machine learning model in order to provide a
-#'  standardized method for predictions for different models.
-#' @description Predictor class description
-#' @field data The training data for the model
-#' @field model The object model
-#' @field task The prediction task the model is trained on
-#' @field class The class of the model
-#' @field prediction.function The prediction function used by the model.
-#' @field batch.size The batch size for the model
-#' @field y The name of the outcome feature in `data`
+#' @title The class for different interpretability methods.
+#' @description Interpreter class description
+#' @field predictor The predictor object to use as a standardised wrapper for the model
+#' @field data.points The indices of the data points used for the PDP/ALE. This
+#' overwrites the "samples" initialization or the batch size variable from the predictor class.
+#' @field functions.1d Functions giving average value across data.points with a given value and feature
+#' @field functions.2d Functions giving 2D pdp functions
+#' @field method The chosen interpretability method for the black-box model.
 #' @examples
-#' data <- MASS::Boston
-#' set.seed(491)
-#' test_ind <- sample(1:nrow(data), nrow(data)%/%5)
-#' train_reg <- data[-test_ind,]
-#' test_reg <- data[test_ind,]
-#'
-#'
-#' data_class <- MASS::crabs
-#' test_ind <- sample(1:nrow(data_class), nrow(data_class)%/%5)
-#' train_class <- data_class[-test_ind,]
-#' test_class <- data_class[test_ind,]
-#'
-#'
-#' linreg <- lm(crim ~., data=train_reg)
-#' linreg_predictor <- Predictor$new(model = linreg, data = train_reg, y="crim",
-#'                                   predict = predict, task = "regression")
-#' actual <- predict(linreg, test_reg)
-#' method <- linreg_predictor$predict(test_reg)
-#' sum(actual != method) # all the same values
-#'
-#' linreg_predictor$print()
+#' Needs to be rewritten
 #' @export
-Predictor <- R6::R6Class("Predictor",
-    public = list(
-    data = NULL,
-    model = NULL,
-    task = NULL,
-    class = NULL,
-    prediction.function = NULL,
-    batch.size = NULL,
-    y = NULL,
+Interpreter <- R6::R6Class(
+  "Interpreter",
+  public = list(
+    predictor = NULL,
+    features = NULL,
+    data.points = NULL,
+    functions.1d = NULL,
+    functions.2d = NULL,
+    method = NULL,
+    # functions.ale = to come later
 
-    #' @param model A trained model.
-    #' @param data A dataframe containing the training data.
-    #' @param predict.func A function to run predictions for the model.
-    #' @param y The name of the outcome variable in data.
-    #' @param task The prediction class, either `classification` or `regression`
-    #' @param class The class of predictions done.
-    #' @param type The type of regression.
-    #' @param batch.size The size of the batch used to create pdp grids
-    #' @return A `Predictor` object.
+    # initialize an interpreter object
+    #' @param predictor The trained predictor object for the model that we want to
+    #'  interpret.
+    #' @param samples The samples used for the PDP/ALE. Overwrites the batch size
+    #' parameter from the predictor object.
+    #' @param data.points The indices of the data points used for the PDP/ALE. This
+    #' overwrites the "samples" parameter or the batch size from the predictor.
+    #' @param method The chosen interpretability method for the black-box model.
+    #' @return An `Interpreter` object.
     #' @note
-    #' A wrapper to pass through an ML algorithm (rpart, etc.) through the
-    #' interpretability functions, the data used to create the algorithm
-    #'
-    #' The necessary variables are model, data, y. The other variables are
-    #' optional, and depend on the use cases. Type should be used only when
-    #' a prediction function is NOT specified.
-    #'
-    #' The outputs of the algorithm must be the values if it is regression, or
-    #' probabilities if classification. For classification problems with more than
-    #' two categories, the output comes out as vectors of probabilities for each
-    #' category. Because this is for ML interpretability, other types of
-    #' predictions (ex: predictions that spit out the factor) are not allowed.
-    initialize = function(model=NULL,
-                          data=NULL,
-                          predict.func=NULL,
-                          y=NULL,
-                          task = NULL,
-                          class=NULL,
-                          type=NULL,
-                          batch.size = 1000) {
-
-      # check that batch size is at least 1 and number
-      checkmate::assert_number(batch.size, lower=1)
-
-      # checks for model input
-      if (is.null(model)){
-        stop("Model not given.")
+    #' PDP and ICE Implementation
+    initialize = function(predictor = NULL,
+                          samples = NULL,
+                          data.points = NULL,
+                          method = "pdp") {
+      # check to see if predictor is a valid predictor object
+      if (is.null(predictor)) {
+        stop("Predictor not given.")
+      }
+      if (!(inherits(predictor, "Predictor"))) {
+        stop("Predictor given is not of the Predictor class.")
       }
 
-      # takes in data, makes sure that it is a dataframe by the end or stops
-      if (is.null(data)){
-        stop("Data not given.")
-        if (!inherits(data, "data.table") || !inherits(data, "data.frame")){
-          stop("Data is not a data.frame or data.table object.")
+      # determine valid features
+      # remove y variable from consideration
+      possible <-
+        names(predictor$data)[names(predictor$data) != predictor$y]
+      # find valid features in the data: more than 1 value
+      possible <-
+        names(which(sapply(
+          sapply(predictor$data[, possible], unique), length
+        ) > 1))
+      # one of the following: int, factor, numeric
+      possible <-
+        possible[which(sapply(predictor$data[, possible], class) %in%
+                         c("numeric", "factor", "integer"))]
+      features <- possible
+
+
+      # check to see if valid sample number
+      # if data.points is not specified
+      data <- predictor$data
+      if (is.null(data.points)) {
+        if (is.null(samples)) {
+          # set samples to either batch size or whole dataset
+          N <- predictor$batch.size
+          N <- min(N, nrow(data))
+          samples <- N
+        } else {
+          # get number of datapoints equal to samples
+          checkmate::assert_numeric(samples)
+          samples <- as.integer(samples)
+          if (samples > nrow(samples) | samples < 1) {
+            stop("Invalid sample number.")
+          }
         }
-      if (inherits(data, "data.table")){
-          data.table::setDF(data)
-        }
+        # set data.points to be worked with (# of datapoints = sample number)
+        data.points <- sample(1:nrow(data), samples)
       }
-
-
-      # checks for valid y input
-      if (is.null(y) || !is.character(y) || !(y%in%names(data))){
-        stop("Y has not been given, is not a character variable, or is not a variable in the given data.")
-      }
-
-      # checks for valid task
-      if (!is.null(task)){
-        if (!(task %in% c("regression", "classification"))){
-          stop("Only regression or classification tasks are supported.")
-        }
-      }
-
-      #' prediction method must be constructed, with optional arguments of
-      #' type and task
-      if (is.null(predict.func)){
-        if (is.null(type)){
-          predict.func.final <- function(model, newdata) predict(model, newdata)
-        } else{
-          predict.func.final <- function(model, newdata) predict(model, newdata, type = type)
-        }
-      } else{
-        predict.func.final <- function(model, newdata){
-          pred <- do.call(predict.func, list(model, newdata = newdata))
-          data.frame(pred, check.names = FALSE)
+      # if data points were specified
+      else {
+        checkmate::assert_numeric(data.points)
+        if (any(!(data.points %in% 1:nrow(data)))) {
+          stop("The data points are not in the training data.")
         }
       }
 
-      self$data <- data
-      self$class <- class
-      self$model <- model
-      self$task <- task
-      self$prediction.function <- predict.func.final
-      self$batch.size <- batch.size
-      self$y <- y
+      # check that method is supported
+      checkmate::assert_character(method)
+      if (!(method %in% c("pdp", "ale"))) {
+        stop("This method is not supported by the function.")
+      }
+
+      # create prediction functions for the features
+      if (method == "pdp") {
+        # for 1-dimensional functions
+        pdp.function <- function(feature) {
+          force(feature)
+          pdp.function.2 <- function(val) {
+            if (length(val) == 1){
+              data <- predictor$data[data.points, , drop = FALSE]
+              data[, feature] <- val
+              mean(predict(predictor, data)[, 1])
+            }
+            else{
+              return.vals <- c()
+              for (v in val){
+                data <- predictor$data[data.points, ,drop = FALSE]
+                data[, feature] <- v
+                return.vals <- c(return.vals, mean(predict(predictor, data)[, 1]))
+              }
+              return.vals
+            }
+          }
+          return(pdp.function.2)
+        }
+
+        functions.pdp <- list()
+        for (feature in features) {
+          temp.list <- list(pdp.function(feature = feature))
+          functions.pdp <- append(functions.pdp, temp.list)
+        }
+        names(functions.pdp) <- features
+
+        # for 2d features
+        pdp.function.2d <- function(feature.1, feature.2){
+          force(feature.1)
+          force(feature.2)
+          pdp.function.2d.2 <- function(val.1, val.2){
+            data <- predictor$data[data.points, , drop = FALSE]
+            data[, feature.1] <- val.1
+            data[, feature.2] <- val.2
+            mean(predict(predictor, data)[,1])
+          }
+          return(pdp.function.2d.2)
+        }
+        functions.pdp.2d <- list()
+        for (feature.1 in features){
+          # list of functions for each
+          temp.list <- list()
+          for (feature.2 in features){
+            if (feature.1 != feature.2){
+              temp <- pdp.function.2d(feature.1 = feature.1, feature.2 = feature.2)
+            }
+            else {
+              temp <- "Please use the one-dimensional functions."
+            }
+            temp.list <- append(temp.list, temp)
+          }
+          names(temp.list) <- features
+          functions.pdp.2d <- append(functions.pdp.2d, list(temp.list))
+        }
+        names(functions.pdp.2d) <- features
+      }
+      else {
+        stop("Other methods have not been implemented yet for general functions.")
+      }
+
+      # initialize all variables belonging to this class
+      self$features <- features
+      self$predictor <- predictor
+      self$data.points <- data.points
+      self$functions.1d <- functions.pdp
+      self$functions.2d <- functions.pdp.2d
+      self$method <- method
     }
-    )
+  )
 )
 
-
-#' @name predict.Predictor
-#' @title Predict method for Predictor class
-#' @description Gives predictions for a predictor object
-#' @param object Predictor object to use.
-#' @param newdata The dataframe to use for the predictions.
-#' @export
-predict.Predictor = function(object, newdata){
-  # check that the new data is a dataframe
-  checkmate::assert_data_frame(newdata)
-  newdata <- as.data.frame(newdata)
-
-  # drop Y variable to make sure that only features are used
-  if (object$y %in% names(newdata)){
-    newdata <- newdata[, -which(names(newdata) == object$y), drop = FALSE]
+# allows user to set data.points
+set.data.points = function(object, data.points) {
+  checkmate::assert_numeric(data.points)
+  if (!(inherits(object, "Interpreter"))) {
+    stop("Object given is not of the Interpreter class.")
   }
-
-  # if we predict 1 target
-  preds <- object$prediction.function(object$model, newdata)
-  preds <- data.frame(preds)
-
-  # check for valid prediction
-  #if (private$predictionCheck == FALSE){
-    # number of rows in predictions
-  #  if (nrow(preds)!= nrow(newdata)){
-  #    stop("Number of predictions do not match the number of observations.")
-  #  }
-  #  # check for missing values
-  #  if (sum(is.na(preds))!=0){
-  #    stop("Predictions have missing values.")
-  #  }
-  #  private$predictionCheck <- TRUE
-  #}
-
-  # Class variable (if classification)
-  if (!is.null(object$class) && ncol(preds) > 1){
-    preds <- preds[, object$class, drop = FALSE]
+  if (any(!(data.points %in% 1:nrow(object$predictor$data)))) {
+    stop("The data points are not in the data.")
   }
-  rownames(preds) <- NULL
-  return(preds)
+  return(Interpreter$new(object$predictor,
+                                object$features,
+                                data.points = data.points,
+                                object$method))
 }
 
-#' @name print.Predictor
-#' @title Printing method for Predictor class
-#' @description Gives information for a predictor object
-#' @param object The Predictor to print
-#' @export
-print.Predictor = function(
-  object
-){
-  cat("Prediction Task:", object$task, "\n")
-  if (object$task == "classification"){
-    cat("Classes: ", paste(unique(object$data[,which(names(object$data)==object$y)]), collapse=" "))
+# allows user to set type of interpretability method
+set.method = function(object, method){
+  checkmate::assert_character(method)
+  if (!(inherits(object, "Interpreter"))) {
+    stop("Object given is not of the Interpreter class.")
   }
+  if (!(method %in% c("pdp", "ale"))){
+    stop("This method is not supported by the function.")
+  }
+  return(Interpreter$new(object$predictor,
+                                object$features,
+                                object$data.points,
+                                method = method))
 }
 
