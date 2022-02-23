ff5a64dae3cb2989288356ff8d430a17b84b8ff8
Theo S <theo_s@berkeley.edu>
2022-02-22 17:20:29 -0800

    * add test for ALE plots

---
+++ tests/testthat/test-ale_plots.R
@@ -0,0 +1,33 @@
+test_that("Tests that the ale plots function is working", {
+  library(interpret)
+  library(Rforestry)
+  set.seed(491)
+
+  data <- MASS::crabs
+  levels(data$sex) <- list(Male = "M", Female = "F")
+  levels(data$sp) <- list(Orange = "O", Blue = "B")
+  colnames(data) <- c("Species","Sex","Index","Frontal Lobe",
+                      "Rear Width", "Carapace Length","Carapace Width","Body Depth")
+
+
+  test_ind <- sample(1:nrow(data), nrow(data)%/%5)
+  train_reg <- data[-test_ind,]
+  test_reg <- data[test_ind,]
+
+  # Train a random forest on the data set
+  forest <- forestry(x=train_reg[,-ncol(train_reg)],
+                     y=train_reg[,ncol(train_reg)])
+
+  # Create a predictor wrapper for the forest
+  forest_predictor <- Predictor$new(model = forest,
+                                    data=train_reg,
+                                    y="Body Depth",
+                                    task = "regression")
+
+  # Initialize an interpreter
+  forest_interpret <- Interpreter$new(predictor = forest_predictor)
+
+
+  p <- plot(forest_interpret, method = "ale", features = c("Frontal Lobe"))
+  expect_equal(names(p),"Frontal Lobe")
+})
