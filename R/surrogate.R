54a603b7f2027f02eaf97297582721e39a3eb2dd
brianc0413 <59323159+brianc0413@users.noreply.github.com>
2022-05-03 12:16:45 -0400

    bugfix for surrogate predictions

--- R/surrogate.R
+++ R/surrogate.R
@@ -107,7 +107,7 @@ predict.Surrogate = function(object,
   # if uncentered, then we need not do anything
   preds <- data.frame(surrogate.preds = rep(object$intercept, nrow(newdata)))
 
-  nonzero <- names(weights)[which(weights != 0)]
+  nonzero <- names(object$weights)[which(object$weights != 0)]
 
   for (feature in nonzero){
     # grid points for this specific feature
