07f956b6202d30607e76adcd132fab64c5807a1e
mattyshen <mattyshen@berkeley.edu>
2023-03-27 23:47:36 -0700

    sb fun

--- tests/testthat/test-pdp_rank.R
+++ tests/testthat/test-pdp_rank.R
@@ -3,23 +3,21 @@ test_that("Tests if the PDP ranking function is working", {
   library(Rforestry)
   set.seed(491)
 
-  data <- MASS::crabs
-  levels(data$sex) <- list(Male = "M", Female = "F")
-  levels(data$sp) <- list(Orange = "O", Blue = "B")
-  colnames(data) <- c("Species","Sex","Index","FrontalLobe",
-                      "RearWidth", "CarapaceLength","CarapaceWidth","BodyDepth")
+  data <-iris
 
   test_ind <- sample(1:nrow(data), nrow(data)%/%5)
   train_reg <- data[-test_ind,]
   test_reg <- data[test_ind,]
+
   # Train a random forest on the data set
-  forest <- forestry(x=train_reg[,-ncol(train_reg)],
-                     y=train_reg[,ncol(train_reg)], seed = 491)
+  forest <- forestry(x=train_reg[,-c(1)],
+                     y=train_reg[,1],
+                     seed = 491)
 
   # Create a predictor wrapper for the forest
   forest_predictor <- Predictor$new(model = forest,
                                     data=train_reg,
-                                    y="BodyDepth",
+                                    y="Sepal.Length",
                                     task = "regression")
 
   # Initialize an interpreter
@@ -28,13 +26,11 @@ test_that("Tests if the PDP ranking function is working", {
   context("Check PDP ranking methodologies without new observation")
   skip_if_not_mac()
   expect_equal(names(sort(pdp.rank(forest_interpret))),
-               c("Sex", "Index", "Species", "RearWidth", "CarapaceWidth",
-                 "CarapaceLength", "FrontalLobe"))
+               c("Sepal.Width", "Species", "Petal.Width", "Petal.Length"))
 
   skip_if_not_mac()
   expect_equal(names(sort(pdp.rank(forest_interpret, rank.method = "FO.Derivative"))),
-               c("Species", "Sex", "Index", "RearWidth", "FrontalLobe",
-                 "CarapaceWidth", "CarapaceLength"))
+               c("Species", "Sepal.Width", "Petal.Width", "Petal.Length"))
 
   context("Check PDP ranking methodologies with new observation")
   new.obs1 <- data[test_ind[1], -which(names(data)==forest_predictor$y)]
@@ -42,36 +38,33 @@ test_that("Tests if the PDP ranking function is working", {
   skip_if_not_mac()
   expect_equal(names(sort(pdp.rank(forest_interpret, rank.method = 'Variance',
                                   new.obs = new.obs1))),
-               c("Sex", "Index", "Species" , "RearWidth", "CarapaceWidth",
-                 "FrontalLobe", "CarapaceLength"))
+               c("Sepal.Width", "Species", "Petal.Width", "Petal.Length"))
+
   skip_if_not_mac()
   expect_equal(names(sort(pdp.rank(forest_interpret, rank.method = 'FO.Derivative',
                                   new.obs = new.obs1))),
-               c("Species", "Sex", "Index", "RearWidth", "FrontalLobe",
-                 "CarapaceWidth", "CarapaceLength"))
+               c("Species", "Sepal.Width", "Petal.Width", "Petal.Length"))
 
   new.obs2 <- data[test_ind[2], -which(names(data)==forest_predictor$y)]
 
   skip_if_not_mac()
   expect_equal(all((pdp.rank(forest_interpret, rank.method = 'FO.Derivative',
                             new.obs = new.obs2)
-                    - c(-1, -1, 0.1485, 0.6345, 0.3823, 0.8468, 0.7364)) < 0.001),
-               TRUE)
+                    - c(0.1027, 0.2607, 0.2830, -1)) < 0.001), TRUE)
 
   skip_if_not_mac()
   expect_equal(all((pdp.rank(forest_interpret, rank.method = 'Variance',
                             new.obs = new.obs2)
-                    - c(0.0496, 0.0004, 0.0444, 1.3000, 0.1870, 1.2186, 0.6162)) < 0.001),
-               TRUE)
+                    - c(0.0257, 0.1215, 0.0632, 0.0325)) < 0.001), TRUE)
 
   context("Check PDP ranking stopping mechanisms")
-  off.obs <- data[test_ind[1], -c(1)]
+  off.obs <- data[test_ind[1], -c(2)]
   two.obs <- data[test_ind[1:2], -which(names(data)==forest_predictor$y)]
   withy.obs <-  data[test_ind[1], ]
-  linear <- lm(BodyDepth ~ ., data = train_reg)
+  linear <- lm(Sepal.Length ~ ., data = train_reg)
   linear_predictor <- Predictor$new(model = linear,
                                     data=train_reg,
-                                    y="BodyDepth",
+                                    y="Sepal.Length",
                                     task = "regression")
   linear_interpret <- Interpreter$new(predictor = linear_predictor)
 
